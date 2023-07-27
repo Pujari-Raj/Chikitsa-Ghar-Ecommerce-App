8e7efa923257a06999d5861fac56d2fd010e6ee3
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-07-27 23:55:20 +0530

    react-router issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes ,Route, Link } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      demo
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
